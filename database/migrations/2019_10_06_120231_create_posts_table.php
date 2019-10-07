011bfc129c9bad2ce81a25343575628f720317e6
Volodymyr Zhonchuk <vzhonchuk@gmail.com>
2019-10-07 23:48:22 +0300

    Post model created and migrated, navigation menu added

---
+++ database/migrations/2019_10_06_120231_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->longText('body');
+            $table->string('slug');
+            $table->boolean('published')->default(false);
+            $table->integer('viewed')->default(0);
+            $table->integer('time_to_read');
+            $table->integer('user_id')->nullable();
+            $table->integer('category_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
