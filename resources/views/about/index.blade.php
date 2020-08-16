1e796fb55ce97b1415ab4936c52e5ba26bf5613c
Volodymyr Zhonchuk <vzhonchuk@gmail.com>
2020-08-16 22:29:58 +0300

    About page tentative

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -10,4 +10,21 @@
     </div>
 </section>
 
+<section class="about-page">
+    <div class="about-page-wrapper">
+        <div class="thumbnail">
+            <img class="lazyload" src="" data-src="" alt="Photo">
+        </div>
+        <div class="about-content">
+        <p>Авиаинсайдер - украинский авиационный новостной ресурс, освещающий
+            события в мире гражданской авиации, анализ рынка отрасли, происшествия,
+            а также скидки и акционные предложения авиакомпаний.</p>
+        </div>
+        <div class="thumbnail">
+            <img class="lazyload" src="" data-src="" alt="Photo">
+        </div>
+        <p></p>
+    </div>
+</section>
+
 @endsection
