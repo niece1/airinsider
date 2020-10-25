b54c77a94ca23f7b10f3dfeb6ed18929b0919ceb
Volodymyr Zhonchuk <vzhonchuk@gmail.com>
2020-10-25 23:38:03 +0200

    Bugsnag added

--- config/app.php
+++ config/app.php
@@ -173,6 +173,7 @@ return [
         /*
          * Application Service Providers...
          */
+        Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
