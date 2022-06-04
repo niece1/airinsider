76d1f6cd464594b976fc6bf9589a059bc37d28d0
Volodymyr Zhonchuk <vzhonchuk@gmail.com>
2022-06-04 21:07:52 +0300

    :chart_with_upwards_trend: GTM
    Google Tag Manager track code added

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -4,6 +4,15 @@
     
     <!-- Head -->
     <head>
+        @env('production')
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NG48L3D');</script>
+        <!-- End Google Tag Manager -->
+        @endenv
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- CSRF Token -->
@@ -16,6 +25,13 @@
     
     <!-- Body -->
     <body>
+        @env('production')
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NG48L3D"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+        @endenv
+        
         @include('cookie-consent::index')
         
         <!-- App -->
