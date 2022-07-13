a7ebb58c728e862d4faa7ab63484863b062d7851
Volodymyr Zhonchuk <vzhonchuk@gmail.com>
2022-07-13 23:39:50 +0300

    :bug: Fix: reactivity issue in livewire
    Reactivity issue fixed in livewire component

--- resources/views/livewire/subscriptions.blade.php
+++ resources/views/livewire/subscriptions.blade.php
@@ -4,14 +4,14 @@
         <div class="newsletter-info">
             <h3>Stay in the know</h3>
             <p>Get updates on industry events and latest news: subscribe to our newsletters.</p>
-            <p>Issued weekly.</p>
+            <p id="test">Issued weekly.</p>
         </div>
         <div class="newsletter-widget">
             <form class="input-wrapper" wire:submit.prevent="store">
                 <div class="form-group">
                     <input id="newsletter" wire:model.defer="email" type="email" placeholder="Your email" required />
                         <x-honey/>
-                        <button type="submit" class={{ $confirmTerms ? '' : 'disabledButton'}} disabled>
+                        <button type="submit" class="{{ $confirmTerms ? 'activeButton' : ''}}" wire:target="confirmTerms" {{ $confirmTerms ? '' : 'disabled'}}>
                             <svg xmlns="http://www.w3.org/2000/svg" class="envelope" viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
