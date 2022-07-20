62aabbfbb030682f1f86eec0cf1245ef453fd618
Volodymyr Zhonchuk <vzhonchuk@gmail.com>
2022-07-20 14:21:05 +0300

    :bug: Fix: pagination caching
    Pagination caching fixed

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use App\Models\Post;
+use App\Observers\PostObserver;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +20,15 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+    ];
+
+    /**
+     * The model observers for your application.
+     *
+     * @var array
+     */
+    protected $observers = [
+        Post::class => [PostObserver::class],
     ];
 
     /**
