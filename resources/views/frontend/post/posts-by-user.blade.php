7e7a8b9f373c7e0239323272ee8b9c68829a0bbf
Volodymyr Zhonchuk <vzhonchuk@gmail.com>
2022-07-21 18:06:57 +0300

    :children_crossing: UI: improvements
    UI improvements

--- resources/views/frontend/post/posts-by-user.blade.php
+++ resources/views/frontend/post/posts-by-user.blade.php
@@ -37,17 +37,25 @@
                     {{ $post_item->excerpt }}{{ $post_item->three_dots }}
                 </p>
                 @if ($post_item->user)
-                <p class="item-blog-author">
-                    <i class="fas fa-user-edit"></i>
+                <div class="item-blog-author">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon-user"
+                        fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8
+                            0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                    </svg>
                     <a href="{{ $post_item->user->slug }}">
                         {{ $post_item->user->name }}
                     </a>
-                </p>
+                </div>
                 @endif
-                <p>
-                    <i class="fas fa-clock"></i>
+                <div class="item-blog-time-to-read">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon-clock"
+                        fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 8v4l3 3m6-3a9
+                            9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
                     {{ $post_item->time_to_read }} minutes to read
-                </p>
+                </div>
                 <p class="item-blog-date">{{ $post_item->date }}</p>
                 <div class="blog-line">
                 </div>
@@ -56,12 +64,17 @@
                         Read more
                     </a>
                     @if ($post_item->category)
-                    <p>
-                        <i class="fas fa-tags"></i>
+                    <div class="item-blog-tag">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon-category"
+                            fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M7 7h.01M7 3h5c.512
+                                0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994
+                                1.994 0 013 12V7a4 4 0 014-4z" />
+                        </svg>
                         <a href="{{ $post_item->category->slug }}">
                             {{ $post_item->category->title }}
                         </a>
-                    </p>
+                    </div>
                     @endif
                 </div>
             </div>
